09a2c8701ce491d0902873641bd2a75f2472ea99
Gabriel Lacerda <gl547842@gmail.com>
2025-09-22 15:43:38 -0300

    Update Professor.java

--- Professor.java
+++ Professor.java
@@ -24,4 +24,28 @@ public class Professor {
         inverseJoinColumns = @JoinColumn(name = "curso_id")
     )
     private Set<Curso> cursos = new HashSet<>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Set<Curso> getCursos() {
+		return cursos;
+	}
+
+	public void setCursos(Set<Curso> cursos) {
+		this.cursos = cursos;
+	}
 }
