345e222d9278eae811f4dbc5a76083ddd7ae2387
Gabriel Lacerda <gl547842@gmai.com>
2025-09-22 15:31:31 -0300

    segundo commit

--- Sistema-de-Gerenciamento-de-Cursos/src/main/java/model/Documento.java
+++ Sistema-de-Gerenciamento-de-Cursos/src/main/java/model/Documento.java
@@ -13,4 +13,23 @@ public class Documento {
     private Long id;
 
     private String nome;
+    
+    @OneToOne(mappedBy = "documento")
+    private Aluno aluno;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 }
