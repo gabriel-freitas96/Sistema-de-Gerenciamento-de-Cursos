fd8280742f53b6b1cc5c92ad7f631d1f7886fa51
Gabriel Lacerda <gl547842@gmail.com>
2025-09-22 15:36:15 -0300

    Update Curso.java

--- Curso.java
+++ Curso.java
@@ -18,4 +18,28 @@ public class Curso {
 
     @ManyToMany(mappedBy = "cursos")
     private Set<Aluno> alunos = new HashSet<>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Set<Aluno> getAlunos() {
+		return alunos;
+	}
+
+	public void setAlunos(Set<Aluno> alunos) {
+		this.alunos = alunos;
+	}
 }
